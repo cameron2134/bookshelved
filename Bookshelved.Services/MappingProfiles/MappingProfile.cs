a84e78d80e845cb562831d09e9036dc650ee7d06
Cameron Dempster <camerondem2134@hotmail.com>
2021-02-28 21:37:32 +0000

    Initial setup

---
+++ Bookshelved.Services/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Bookshelved.Core.DomainModels.Book;
+using Bookshelved.Core.DTOs;
+using Bookshelved.Core.DTOs.Book;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshelved.Services.MappingProfiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookDTO>()
+                .ReverseMap();
+
+            CreateMap<Book, BookDetailsDTO>()
+                .ReverseMap();
+
+            CreateMap<Series, SeriesDTO>()
+                .ReverseMap();
+
+            CreateMap<Author, AuthorDTO>()
+                .ReverseMap();
+        }
+    }
+}
---
+++ Bookshelved.Web/Startup.cs
@@ -0,0 +1,73 @@
+using Bookshelved.Core.Interfaces.Repos;
+using Bookshelved.Repository;
+using Bookshelved.Services;
+using Bookshelved.Services.MappingProfiles;
+using Bookshelved.Web.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookshelved.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            var connectionString = Configuration.GetConnectionString("BookshelfDB");
+
+            services.AddDbContext<BookshelfContext>(o => o.UseSqlServer(connectionString));
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
+            services.AddSingleton<WeatherForecastService>();
+            services.AddTransient<BookService>();
+            services.AddAutoMapper(typeof(MappingProfile));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
